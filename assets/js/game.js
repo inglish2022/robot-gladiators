c7e6549c5a7886fe862b8b506d78ef801250bb95
Inglish Foust <inglish.foust5@gmail.com>
2022-03-06 19:28:12 -0500

    adding to js

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,25 @@
-window.alert("This is an alert!  JavaScript is running!");
+function fight()  {
+    window.alert("The fight has begun!");
+}
+
+
+var playerName = window.prompt("What is your robot's name?");
+// What is this?
+console.log(playerName);
+
+console.log("This logs a string, good for leaving yourself a message");
+// this will do mathand log 20
+console.log(10+10);
+//what is this?
+console.log("Our robots name is " + playerName);
+
+var name = "your name";
+console.log(name);
+
+
+
+
+
+
+// fight();
+
